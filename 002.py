15ec378213468d2121c9d5816a7085e8dae5c324
gabill <evelyngabill96@gmail.com>
2021-02-03 18:50:59 -0300

    questao2

---
+++ 002.py
@@ -0,0 +1,19 @@
+#op = input('Digite a operação: ')
+#print('Operação: {}!'.format(op))
+
+#n1 = int(input('n1: '))
+#n2 = int(input('n2: '))
+#soma = n1+n2
+#print('soma entre',n1,'e',n2,':{}'.format(soma))
+#print('soma entre {} e {} vale {}'.format(n1, n2, soma))
+
+
+n = input('Digite algo: ')
+print('é alfanumerico? ')
+print(n.isalnum())
+print('é letra?')
+print(n.isalpha())
+print('é numero?') 
+print(n.isnumeric())
+print('è tudo maiusculo?')
+print(n.isupper())
